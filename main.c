955f0f29822ce40752448430a906e5c9aaa379b4
anticomu <malerbafrancesco206@gmail.com>
2026-04-04 21:09:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <pspkernel.h>
-#include <pspdebug.h>
 #include <pspdisplay.h>
 #include <pspgu.h>
+#include <pspgum.h>
 #include <pspctrl.h>
+#include <pspdebug.h>
 #include <math.h>
+#include <stdio.h>
 
-PSP_MODULE_INFO("LFS_STABLE", 0, 1, 1);
-PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
+PSP_MODULE_INFO("LFS_PRO", 0, 1, 1);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
 static unsigned int __attribute__((aligned(16))) list[262144];
 
@@ -24,52 +26,73 @@ int setup_callbacks(void) {
     return thid;
 }
 
-int main() {
-    setup_callbacks();
-    pspDebugScreenInit();
+typedef struct { float x, y, z; } Vertex;
 
-    // Inizializzazione Grafica
+void init_graphics() {
     sceGuInit();
     sceGuStart(GU_DIRECT, list);
     sceGuDrawBuffer(GU_PSM_8888, (void*)0, 512);
     sceGuDispBuffer(480, 272, (void*)0x88000, 512);
-    sceGuDisplay(GU_TRUE);
+    sceGuDepthBuffer((void*)0x110000, 512);
+    sceGuOffset(2048 - (480/2), 2048 - (272/2));
+    sceGuViewport(2048, 2048, 480, 272);
+    sceGuDepthRange(65535, 0);
+    sceGuScissor(0, 0, 480, 272);
+    sceGuEnable(GU_SCISSOR_TEST);
+    sceGuEnable(GU_DEPTH_TEST);
+    sceGuDepthFunc(GU_GEQUAL);
+    sceGuFrontFace(GU_CCW);
+    sceGuShadeModel(GU_SMOOTH);
     sceGuFinish();
     sceGuSync(0, 0);
+    sceDisplayWaitVblankStart();
+    sceGuDisplay(GU_TRUE);
+}
 
-    float speed = 0.0f;
-    float rpm = 1000.0f;
+int main() {
+    setup_callbacks();
+    pspDebugScreenInit();
+    init_graphics();
+
+    float car_x = 0, car_z = 0, car_angle = 0, speed = 0;
     SceCtrlData pad;
 
     while(1) {
         sceCtrlReadBufferPositive(&pad, 1);
+        if (pad.Buttons & PSP_CTRL_CROSS) speed += 0.05f;
+        if (pad.Lx < 80) car_angle -= 0.04f;
+        if (pad.Lx > 170) car_angle += 0.04f;
+        speed *= 0.96f;
+        car_x += sinf(car_angle) * speed;
+        car_z += cosf(car_angle) * speed;
 
-        // Fisica Auto
-        if (pad.Buttons & PSP_CTRL_CROSS) speed += 0.05f; // Accelera
-        speed *= 0.98f; // Attrito
-        rpm = 1000.0f + (speed * 50.0f);
-
-        // Rendering
         sceGuStart(GU_DIRECT, list);
-        sceGuClearColor(0xff444444); // Asfalto
-        sceGuClear(GU_COLOR_BUFFER_BIT);
+        sceGuClearColor(0xff222222);
+        sceGuClear(GU_COLOR_BUFFER_BIT | GU_DEPTH_BUFFER_BIT);
+
+        // Setup Proiezione 3D
+        sceGumMatrixMode(GU_PROJECTION);
+        sceGumLoadIdentity();
+        sceGumPerspective(75.0f, 480.0f/272.0f, 0.5f, 1000.0f);
+
+        // Setup Telecamera (Inseguimento)
+        sceGumMatrixMode(GU_VIEW);
+        sceGumLoadIdentity();
+        ScePspFVector3 cam_pos = { car_x - sinf(car_angle)*8, 4.0f, car_z - cosf(car_angle)*8 };
+        ScePspFVector3 cam_look = { car_x, 1.0f, car_z };
+        ScePspFVector3 cam_up = { 0, 1, 0 };
+        sceGumLookAt(&cam_pos, &cam_look, &cam_up);
+
         sceGuFinish();
         sceGuSync(0, 0);
 
-        // Interfaccia (HUD)
         pspDebugScreenSetXY(2, 2);
-        pspDebugScreenPrintf("LFS PSP - ENGINE RUNNING");
+        pspDebugScreenPrintf("LFS PSP - 3D READY");
         pspDebugScreenSetXY(2, 4);
-        pspDebugScreenPrintf("VELOCITA': %d KM/H", (int)(speed * 10));
-        pspDebugScreenSetXY(2, 5);
-        pspDebugScreenPrintf("MOTORE:    %d RPM", (int)rpm);
-        
-        if (pad.Buttons & PSP_CTRL_SELECT) break;
+        pspDebugScreenPrintf("SPEED: %d KM/H | ANGLE: %.2f", (int)(speed*20), car_angle);
 
         sceDisplayWaitVblankStart();
         sceGuSwapBuffers();
     }
-
-    sceKernelExitGame();
     return 0;
 }
