8d1febedf5adaf529a30310e674afd52a44192db
anticomu <malerbafrancesco206@gmail.com>
2026-04-07 02:10:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,155 +1,102 @@
+#include "raylib.h"
 #include <pspkernel.h>
-#include <pspdisplay.h>
-#include <pspgu.h>
-#include <pspgum.h>
-#include <pspctrl.h>
 #include <pspdebug.h>
 #include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <malloc.h>
 
-PSP_MODULE_INFO("LFS_PSP", 0, 1, 1);
+/* PSP Module Info */
+PSP_MODULE_INFO("LFS_PSP_RAYLIB", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
-static unsigned int __attribute__((aligned(16))) list[262144];
+/* PSP Callbacks */
+int exit_callback(int arg1, int arg2, void *common) {
+    sceKernelExitGame();
+    return 0;
+}
 
-typedef struct {
-    unsigned int color;
-    float x, y, z;
-} Vertex;
-
-Vertex* car_vertices = NULL;
-int vertex_count = 0;
-
-int exit_callback(int arg1, int arg2, void *common) { sceKernelExitGame(); return 0; }
 int callback_thread(SceSize args, void *argp) {
     int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
     sceKernelRegisterExitCallback(cbid);
     sceKernelSleepThreadCB();
     return 0;
 }
+
 int setup_callbacks(void) {
     int thid = sceKernelCreateThread("update_thread", callback_thread, 0x11, 0xFA0, 0, 0);
-    if (thid >= 0) sceKernelStartThread(thid, 0, 0);
+    if (thid >= 0) {
+        sceKernelStartThread(thid, 0, 0);
+    }
     return thid;
 }
 
-void load_obj(const char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (!file) return;
-    char line[256];
-    int total_v = 0, total_f = 0;
-    while (fgets(line, sizeof(line), file)) {
-        if (line[0] == 'v' && line[1] == ' ') total_v++;
-        else if (line[0] == 'f') total_f++;
-    }
-    Vertex* temp_v = (Vertex*)malloc(total_v * sizeof(Vertex));
-    fseek(file, 0, SEEK_SET);
-    int v_idx = 0;
-    float min_x = 1e6, max_x = -1e6, min_y = 1e6, max_y = -1e6, min_z = 1e6, max_z = -1e6;
-    while (fgets(line, sizeof(line), file)) {
-        if (line[0] == 'v' && line[1] == ' ') {
-            temp_v[v_idx].color = 0xFFFFFFFF;
-            sscanf(line, "v %f %f %f", &temp_v[v_idx].x, &temp_v[v_idx].y, &temp_v[v_idx].z);
-            if(temp_v[v_idx].x < min_x) min_x = temp_v[v_idx].x; if(temp_v[v_idx].x > max_x) max_x = temp_v[v_idx].x;
-            if(temp_v[v_idx].y < min_y) min_y = temp_v[v_idx].y; if(temp_v[v_idx].y > max_y) max_y = temp_v[v_idx].y;
-            if(temp_v[v_idx].z < min_z) min_z = temp_v[v_idx].z; if(temp_v[v_idx].z > max_z) max_z = temp_v[v_idx].z;
-            v_idx++;
-        }
-    }
-    float center_x = (min_x + max_x) / 2.0f, center_y = min_y, center_z = (min_z + max_z) / 2.0f;
-    float max_dim = (max_x-min_x > max_z-min_z) ? (max_x-min_x) : (max_z-min_z);
-    float scale = 4.0f / (max_dim > 0.1f ? max_dim : 1.0f);
-    for(int i=0; i<v_idx; i++) {
-        temp_v[i].x = (temp_v[i].x - center_x) * scale;
-        temp_v[i].y = (temp_v[i].y - center_y) * scale;
-        temp_v[i].z = (temp_v[i].z - center_z) * scale;
-    }
-    vertex_count = total_f * 3;
-    car_vertices = (Vertex*)memalign(16, vertex_count * sizeof(Vertex));
-    fseek(file, 0, SEEK_SET);
-    int f_idx = 0;
-    while (fgets(line, sizeof(line), file)) {
-        if (line[0] == 'f') {
-            int v1, v2, v3;
-            if (sscanf(line, "f %d %d %d", &v1, &v2, &v3) == 3) {
-                car_vertices[f_idx++] = temp_v[v1-1]; car_vertices[f_idx++] = temp_v[v2-1]; car_vertices[f_idx++] = temp_v[v3-1];
-            }
-        }
-    }
-    free(temp_v); fclose(file);
-}
-
-int main() {
+int main(void) {
     setup_callbacks();
-    load_obj("car.obj");
-    sceGuInit();
-    sceGuStart(GU_DIRECT, list);
-    sceGuDrawBuffer(GU_PSM_8888, (void*)0, 512);
-    sceGuDispBuffer(480, 272, (void*)0x88000, 512);
-    sceGuDepthBuffer((void*)0x110000, 512);
-    sceGuOffset(2048 - (480/2), 2048 - (272/2));
-    sceGuViewport(2048, 2048, 480, 272);
-    sceGuDepthRange(0, 65535);
-    sceGuScissor(0, 0, 480, 272);
-    sceGuEnable(GU_SCISSOR_TEST);
-    sceGuEnable(GU_DEPTH_TEST);
-    sceGuDepthFunc(GU_ALWAYS); // Disegna SEMPRE per il test
-    sceGuDisable(GU_CULL_FACE);
-    sceGuFinish(); sceGuSync(0, 0); sceDisplayWaitVblankStart(); sceGuDisplay(GU_TRUE);
 
-    float px = 0, pz = 0, angle = 0, speed = 0;
-    SceCtrlData pad;
-    while(1) {
-        sceCtrlReadBufferPositive(&pad, 1);
-        if (pad.Buttons & PSP_CTRL_CROSS) speed += 0.05f;
-        if (pad.Buttons & PSP_CTRL_SQUARE) speed -= 0.08f;
-        if (pad.Lx < 80) angle -= 0.05f;
-        if (pad.Lx > 170) angle += 0.05f;
-        speed *= 0.96f;
-        px += sinf(angle) * speed; pz += cosf(angle) * speed;
+    // Initialization
+    const int screenWidth = 480;
+    const int screenHeight = 272;
 
-        sceGuStart(GU_DIRECT, list);
-        sceGuClearColor(0xFFFFBB66); 
-        sceGuClear(GU_COLOR_BUFFER_BIT | GU_DEPTH_BUFFER_BIT);
-        
-        sceGumMatrixMode(GU_PROJECTION); sceGumLoadIdentity(); sceGumPerspective(75.0f, 480.0f/272.0f, 0.1f, 1000.0f);
-        sceGumMatrixMode(GU_VIEW); sceGumLoadIdentity();
-        ScePspFVector3 cam_p = { px - sinf(angle)*15, 8.0f, pz - cosf(angle)*15 };
-        ScePspFVector3 cam_l = { px, 1.0f, pz };
-        ScePspFVector3 cam_u = { 0, 1, 0 };
-        sceGumLookAt(&cam_p, &cam_l, &cam_u);
+    InitWindow(screenWidth, screenHeight, "LFS PSP - Raylib Edition");
 
-        // Erba (Verde scuro)
-        sceGumMatrixMode(GU_MODEL); sceGumLoadIdentity();
-        Vertex ground[6] __attribute__((aligned(16))) = { 
-            {0xFF004400, -500,0,-500}, {0xFF004400, 500,0,-500}, {0xFF004400, 500,0, 500}, 
-            {0xFF004400, 500,0, 500}, {0xFF004400, -500,0, 500}, {0xFF004400, -500,0,-500} 
-        };
-        sceGuDrawArray(GU_TRIANGLES, GU_COLOR_8888|GU_VERTEX_32BITF|GU_TRANSFORM_3D, 6, 0, ground);
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
-        // Strada (Grigio asfalto)
-        Vertex road[6] __attribute__((aligned(16))) = { 
-            {0xFF333333, -10,0.1f,-500}, {0xFF333333, 10,0.1f,-500}, {0xFF333333, 10,0.1f, 500}, 
-            {0xFF333333, 10,0.1f, 500}, {0xFF333333, -10,0.1f, 500}, {0xFF333333, -10,0.1f,-500} 
-        };
-        sceGuDrawArray(GU_TRIANGLES, GU_COLOR_8888|GU_VERTEX_32BITF|GU_TRANSFORM_3D, 6, 0, road);
+    // Load car model
+    Model car = LoadModel("car.obj");
+    Vector3 carPosition = { 0.0f, 0.0f, 0.0f };
+    float carRotation = 0.0f;
+    float carSpeed = 0.0f;
 
-        // Auto (Bianca)
-        if (car_vertices) {
-            sceGumMatrixMode(GU_MODEL); sceGumLoadIdentity();
-            ScePspFVector3 p = { px, 0.5f, pz }; sceGumTranslate(&p); sceGumRotateY(angle);
-            sceGuDrawArray(GU_TRIANGLES, GU_COLOR_8888|GU_VERTEX_32BITF|GU_TRANSFORM_3D, vertex_count, 0, car_vertices);
-        }
+    SetTargetFPS(60);
 
-        sceGuFinish(); sceGuSync(0, 0);
-        
-        pspDebugScreenSetXY(0,0);
-        pspDebugScreenPrintf("LFS PSP - POS: %.2f, %.2f SPD: %.2f", px, pz, speed);
-        
-        sceDisplayWaitVblankStart();
-        sceGuSwapBuffers();
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // Update
+        if (IsKeyDown(KEY_UP)) carSpeed += 0.1f;
+        if (IsKeyDown(KEY_DOWN)) carSpeed -= 0.1f;
+        if (IsKeyDown(KEY_LEFT)) carRotation += 2.0f;
+        if (IsKeyDown(KEY_RIGHT)) carRotation -= 2.0f;
+
+        // Friction
+        carSpeed *= 0.98f;
+
+        // Move car
+        carPosition.x += carSpeed * sinf(carRotation * DEG2RAD);
+        carPosition.z += carSpeed * cosf(carRotation * DEG2RAD);
+
+        // Update Camera to follow car
+        camera.target = carPosition;
+        camera.position.x = carPosition.x - 10.0f * sinf(carRotation * DEG2RAD);
+        camera.position.z = carPosition.z - 10.0f * cosf(carRotation * DEG2RAD);
+        camera.position.y = 5.0f;
+
+        // Draw
+        BeginDrawing();
+            ClearBackground(SKYBLUE);
+
+            BeginMode3D(camera);
+                // Draw ground
+                DrawGrid(20, 1.0f);
+                
+                // Draw car model
+                DrawModelEx(car, carPosition, (Vector3){ 0, 1, 0 }, carRotation, (Vector3){ 1, 1, 1 }, WHITE);
+                
+            EndMode3D();
+
+            DrawFPS(10, 10);
+            DrawText("LFS PSP - Raylib Professional Engine", 10, 30, 20, BLACK);
+            DrawText(TextFormat("Speed: %.2f", carSpeed), 10, 60, 20, MAROON);
+
+        EndDrawing();
     }
+
+    // De-Initialization
+    UnloadModel(car);
+    CloseWindow();
+
     return 0;
 }
